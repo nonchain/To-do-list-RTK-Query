a0b099d2eec0a96f637d2ef0163eafd5f1e52925
nonchain <code.progali@gamil.com>
2022-12-14 15:45:46 +0330

    Add to-do data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1 className="text-2xl font-bold">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
