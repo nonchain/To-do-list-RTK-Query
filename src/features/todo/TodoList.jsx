089ee37d7de11d3fb72e924696886cf9bad8c656
nonchain <code.progali@gamil.com>
2022-12-14 16:38:12 +0330

    Get data from db

--- src/features/todo/TodoList.jsx
+++ src/features/todo/TodoList.jsx
@@ -14,29 +14,32 @@ const TodoList = () => {
    }
 
    const newItemSection =
-      <form onSubmit={handleSubmit}>
-         <label htmlFor="new-todo">Enter a new todo item</label>
-         <div className="new-todo">
+      <form onSubmit={handleSubmit} className="mt-3 flex flex-col gap-2">
+         <label htmlFor="new-todo" >Enter a new todo item</label>
+         <div className="flex gap-3 items-center">
             <input
+               className="px-3 py-2 rounded border border-gray-400 focus:border-gray-800 duration-200"
                type="text"
                id="new-todo"
                value={newTodo}
                onChange={(e) => setNewTodo(e.target.value)}
                placeholder="Enter new todo"
             />
+            <button className="rounded-full w-9 h-9 flex items-center justify-center bg-blue-500 text-white">
+               <i className="ri-add-line"></i>
+            </button>
          </div>
-         <button className="submit">
-            <i className="ri-upload-cloud-2-line"></i>
-         </button>
+
       </form>
 
 
    let content;
-   // Define conditional content
+   if (isLoading) content = <p>Loading...</p>
+   if (isSuccess) content = JSON.stringify(todos);
 
    return (
-      <main>
-         <h1>Todo List</h1>
+      <main className="flex flex-col p-8 gap-8">
+         <h1 className="text-4xl text-slate-800 font-semibold">Todo List</h1>
          {newItemSection}
          {content}
       </main>
