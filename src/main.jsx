ed96be56bb10838fac2af9e89b5caa59af04c0f6
nonchain <code.progali@gamil.com>
2022-12-14 16:13:20 +0330

    Import ApiProvider and apiSlice

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import App from './App'
 import './index.css';
 import 'remixicon/fonts/remixicon.css';
 
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { apiSlice } from './features/api/apiSlice';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>,
 )
